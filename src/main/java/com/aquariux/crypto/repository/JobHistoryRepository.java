46d980e86d1c1165f95ed03b580b9c6c01959b60
Hieu Nguyen <9hieu_nguyen@singaporeair.com.sg>
2023-11-03 16:54:05 +0700

    create app jdl to create new entity

---
+++ src/main/java/com/aquariux/crypto/repository/JobHistoryRepository.java
@@ -0,0 +1,12 @@
+package com.aquariux.crypto.repository;
+
+import com.aquariux.crypto.domain.JobHistory;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the JobHistory entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface JobHistoryRepository extends JpaRepository<JobHistory, Long> {}
